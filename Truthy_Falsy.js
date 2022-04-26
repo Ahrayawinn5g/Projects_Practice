845f6ef1128472725098c99d8b81f3d0822cff77
Ahraya <Ahrayawinn5g@gmail.com>
2022-04-26 13:56:27 -0400

    Inherent truth-y and false-y values examples

---
+++ Truthy_Falsy.js
@@ -0,0 +1,38 @@
+const userInput = prompt("Enter something")
+
+if (userInput) {
+    console.log("Truth-y")
+} else {
+    console.log("False-y")
+}
+
+if (0) {
+    console.log("Truth-y")
+} else {
+    console.log("False-y")
+}
+
+if (NaN) {
+    console.log("Truth-y")
+} else {
+    console.log("False-y")
+}
+
+if (9000) {
+    console.log("Truth-y")
+} else {
+    console.log("False-y")
+}
+
+if (null) {
+    console.log("Truth-y")
+} else {
+    console.log("False-y")
+}
+
+if (undefined) {
+    console.log("Truth-y")
+} else {
+    console.log("False-y")
+}
+
