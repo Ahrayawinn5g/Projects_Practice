11a9e6a18bfd46c417972ff2cfb65ae9dcf9c694
Ahraya <Ahrayawinn5g@gmail.com>
2022-05-12 16:12:19 -0400

    For loops practice with examples

--- ForLoops.js
+++ ForLoops.js
@@ -9,10 +9,32 @@
 // console.log(9)
 // console.log(10)
 
+// increment by 1 until reaching 10
 for (let i = 1; i <= 10; i++) {
-    console.log(i);
+    console.log(i)
 }
 
+// Prints string 6 times 
 for (let song = 1; song <= 6; song++) {
-    console.log("Da ba dee da ba daa");
+    console.log("Da ba daa de dum")
+}
+
+// increments by 2 starting from 0 until 20 giving only even numbers 
+for (let e = 0; e <= 20; e += 2) {
+    console.log(e)
+}
+
+// increments by 2 starting from 1 until 20 giving odd numbers 
+for (let o = 1; e <= 20; e += 2) {
+    console.log(o)
+}
+
+// Subtracts from 100 until reaching 0 by increments of 10
+for (let s = 100; s >= 0; s -= 10) {
+    console.log(s)
+}
+
+// Multiply by 10 until reaching 1000
+for (let m = 10; m <= 1000; m *= 10) {
+    console.log(m)
 }
