72fd2b50b0abe9f6ddf1569a11b3ea1f08af19fd
Ahraya Winn <Ahrayawinn5g@gmail.com>
2022-05-22 14:34:29 -0400

    Todo List project in Browser

--- ForOfLoops.js
+++ ForOfLoops.js
@@ -1,17 +1,17 @@
-// const subreddits = ['cringe', 'books', 'chickens', 'anime', 'sports', 'luffy']
+const subreddits = ['cringe', 'books', 'chickens', 'anime', 'sports', 'luffy']
 
-// for(let i = 0; i < subreddits.length; i++) {
-//     console.log(`Visit reddit.com/r/${subreddits[i]}`)
-// }
+for(let i = 0; i < subreddits.length; i++) {
+    console.log(`Visit reddit.com/r/${subreddits[i]}`)
+}
 
-// for(let sub of subreddits) {
-//     console.log(`Visit reddit.com/r/${sub}`)
-// }
+for(let sub of subreddits) {
+    console.log(`Visit reddit.com/r/${sub}`)
+}
 
 
-// for(let char of "hello world") {
-//     console.log(char)
-// }
+for(let char of "hello world") {
+    console.log(char)
+}
 
 const numbers = [1,2,3,4,5,6,7,8,9];
 
